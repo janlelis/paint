61507563738abf9e2733acafe2506c53186d4509
Jeff Dickey <jdickey@seven-sigma.com>
2018-12-17 02:47:24 +0800

    Fix Paint::RGB_COLORS constant generation
    Generation of `Paint::RGB_COLORS` runs `Marshal.load` on a gunzipped
    list of X11's `rgb.txt` color list. The existing code makes use of
    the RubyGems API [`Gem.gunzip`](http://docs.seattlerb.org/rubygems/Gem.html#method-c-gunzip)
    method. Calling this method now produces a deprecation warning:
    
    ```
    NOTE: Gem.gunzip is deprecated; use Gem::Util.gunzip instead. It will be removed on or after 2018-12-01.
    Gem.gunzip called from /Users/jeffdickey/src/ruby/paint/lib/paint/rgb_colors.rb:5.
    ```
    
    This commit makes the indicated change to call
    [`Gem::Util.gunzip`](http://docs.seattlerb.org/rubygems/Gem/Util.html#method-c-gunzip).

--- lib/paint/rgb_colors.rb
+++ lib/paint/rgb_colors.rb
@@ -2,7 +2,7 @@ module Paint
   # A list of color names, based on X11's rgb.txt
   RGB_COLORS =
     Marshal.load(
-      Gem.gunzip(
+      Gem::Util.gunzip(
         File.binread(
           File.dirname(__FILE__) + '/../../data/rgb_colors.marshal.gz'
         )
